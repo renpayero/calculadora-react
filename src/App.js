662d77a08653c4bf3c03c71f955f2ee1f6313ad2
Renzo Payero <renzopayero@hotmail.com>
2023-10-01 02:12:13 -0300

    acomode las dimensiones de la imagen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
+import dakodev from './imagenes/dakodev.png'
 
 function App() {
   return (
     <div className="App">
-
+      <div className='logoDakodevContenedor'>
+        <img className='logoDakodev' src={dakodev} alt='logoDakodev' />
+      </div>
+      <div className='contenedorCalculadora'>
+        <div className='fila'>fila</div>
+        <div className='fila'>fila</div>
+        <div className='fila'>fila</div>
+        <div className='fila'>fila</div>
+        <div className='fila'>fila</div>
+      </div>
     </div>
   );
 }
---
+++ src/componentes/Boton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Boton(props) {
+  return (
+    <div>
+        {props.children}
+    </div>
+  );
+}
